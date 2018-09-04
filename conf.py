658c7b448f0ca78b5ea483252130e33c246aaf13
DP <32961877+bezcreative@users.noreply.github.com>
2018-09-04 15:47:50 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -104,11 +104,11 @@ html_logo = './Disciple_Tools_Theme/images/dt_logo_1.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["../css"]
+html_static_path = ['_static']
 
 def setup(app):
         app.add_stylesheet("css/custom.css")
-
+        
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
