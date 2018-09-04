0ebd03cd59d8e8e78fe2a6fd9a363b659f601106
DP <32961877+bezcreative@users.noreply.github.com>
2018-09-04 15:39:15 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -104,7 +104,7 @@ html_logo = './Disciple_Tools_Theme/images/dt_logo_1.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["../css"]
 
 def setup(app):
         app.add_stylesheet("css/custom.css")
